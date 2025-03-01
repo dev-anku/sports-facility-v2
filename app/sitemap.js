11534130afaf3db1fc08f9e1b86e9fbe43e4e236
Anku <ankitkumarmuhal3@gmail.com>
2025-03-01 11:09:40 +0530

    Added sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,16 @@
+export async function GET() {
+  const baseUrl = "https://sports-facility.in";
+
+  const pages = ["", "about", "sports", "services", "projects", "contact"].map(
+    (page) => `${baseUrl}/${page}`,
+  );
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${pages.map((url) => `<url><loc>${url}</loc></url>`).join("")}
+    </urlset>`;
+
+  return new Response(sitemap, {
+    headers: { "Content-Type": "application/xml" },
+  });
+}
